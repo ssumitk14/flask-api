654b965e5a0a112d6f7acce5b8ec14552a6f1f5f
ssumitk14 <ssumitk14@gmail.com>
2021-07-17 16:07:41 +0530

    Passing Arguments

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
+names = {"sumit":{"age":23, "gender":"male"},
+        "amit":{"age":26,"gender":"male"}}
+
 class className(Resource):
-    def get(self):
-        return {"name":"sumit"}
-    def post(self):
-        return {"name":"Posted"}
-api.add_resource(className,'/test')
+    def get(self,name):
+        return names[name]
+
+api.add_resource(className,"/test/<string:name>")
 
 if __name__ == "__main__":
     app.run(debug=True)
--- test.py
+++ test.py
@@ -2,5 +2,5 @@ import requests
 
 baseURL = "http://127.0.0.1:5000/"
 
-data = requests.get(baseURL+'/test')
+data = requests.get(baseURL+'/test/sumit')
 print(data.json())
